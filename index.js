0907d54ca2bb4766f89ec468f4c6463d44d5cb59
Waleed <32379213+WallyThePenguin@users.noreply.github.com>
2018-12-02 15:11:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const { Client } = require('klasa');
 Client.use(require('klasa-bot-plugin'));
 Client.defaultPermissionLevels
     // Make the requirements to use the conf command stricter than just who can add the bot to the guild
-    .add(6, (client, message) => message.guild && message.member.permissions.has('ADMINISTRATOR'), { fetch: true })
+    .add(6, (message) => message.guild && message.member.permissions.has('ADMINISTRATOR'), { fetch: true })
     // add a role above guild owners that let your support team help setup/troubleshoot on other guilds.
-    .add(8, (client, message) => botSupportTeam.includes(message.author.id))
+    .add(8, (message) => botSupportTeam.includes(message.author.id))
 new Client({
     fetchAllMembers: false,
     defaultPermissionLevels: true,
